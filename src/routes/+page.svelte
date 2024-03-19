abfd38f916aadd7e0c08a4eff69eba5a455f869e
poloman719 <poloman7190@gmail.com>
2024-03-18 19:22:43 -0700

    not much

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { onMount } from 'svelte';
 	import Header from './Header.svelte';
 </script>
 
@@ -6,7 +7,10 @@
 	<Header />
 	<div class="bg-gray flex-grow h-full pt-10 flex justify-center">
 		<div class="w-[50%]">
-			<div class="w-full bg-white aspect-[8.5/11]"></div>
+			<div class="w-full bg-white aspect-[8.5/11]">
+				<div class="p-10"></div> 
+				<!-- figure out how to insert bold/italic/underlined text as the cursor moves -->
+			</div>
 		</div>
 	</div>
 </div>
